0b41ece792f8b7ad980b6f315a38e779a5b613b2
Raul Vasile <raul.vasile@everymatrix.com>
2024-06-10 12:49:25 +0300

    nevermind

--- index.js
+++ index.js
@@ -48,11 +48,17 @@ const judete = [
   'https://prezenta.roaep.ro/europarlamentare09062024/data/json/sicpv/pv/pv_vl_prov.json',
   'https://prezenta.roaep.ro/europarlamentare09062024/data/json/sicpv/pv/pv_vs_prov.json',
   'https://prezenta.roaep.ro/europarlamentare09062024/data/json/sicpv/pv/pv_vn_prov.json',
+  'https://prezenta.roaep.ro/europarlamentare09062024/data/json/sicpv/pv/pv_sr_prov.json'
 ];
 
 let total = [];
 let count = 0;
 let total_voturi_v = [];
+let total_nule = [];
+
+// const total_prezenta = 9565155;
+const total_prezenta = 9661773;
+
 
 const getData = (url) => {
   return new Promise((resolve, reject) => {
@@ -65,21 +71,27 @@ const getData = (url) => {
       .then((data) => {
         let total_votes = 0;
         let total_voturi_valide = 0;
+        let total_voturi_nule = 0;
         const table = data.stages.PROV?.scopes?.PRCNCT?.categories?.EUP?.table;
 
         if (table) {
           Object.keys(table).forEach((item) => {
             let voturi_nicu = table[item].votes.filter((tab) => tab.candidate == 'ȘTEFĂNUȚĂ NICOLAE-BOGDĂNEL');
+            // let voturi_nicu = table[item].votes.filter((tab) => tab.candidate == 'GHEORGHE VLAD-DAN');
             total_votes += parseInt(voturi_nicu[0].votes, 10);
 
             let voturi_valide = table[item].fields.filter((tab) => tab.name == 'e');
             total_voturi_valide += parseInt(voturi_valide[0].value);
+
+            let voturi_nule = table[item].fields.filter((tab) => tab.name == 'f');
+            total_voturi_nule += parseInt(voturi_valide[0].value);
 
           });
         }
 
         console.log(`total_votes ${judet[1]} = ${total_votes} | total_voturi_valide ${total_voturi_valide} | procent judet: ${parseFloat(total_votes * 100 / total_voturi_valide).toFixed(2)}%`);
         total_voturi_v.push(total_voturi_valide);
+        total_nule.push(total_voturi_nule);
 
         total.push(total_votes);
         count++;
@@ -87,10 +99,14 @@ const getData = (url) => {
 
           let total_voturi = total.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
           let t_v_v = total_voturi_v.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+          let total_n = total_nule.reduce((acc, curr) => acc + curr, 0);
 
           console.log('TOTAL VOTURI NS = ', total_voturi.toLocaleString('en-US'));
           console.log('TOTAL VOTURI VALIDE = ', t_v_v.toLocaleString('en-US'));
-          console.log(`Procent:  ${parseFloat((total_voturi / t_v_v) * 100).toFixed(2)}%`);
+          console.log(`Procent:  ${parseFloat((total_voturi / t_v_v) * 100).toFixed(4)}%`);
+          console.log(`Procent voturi numarate (aproximativ): ${parseFloat((t_v_v / total_prezenta) * 100).toFixed(4)}%`);
+          // console.log(`Numar voturi nule: ${total_n}`);
+          // console.log(`Procent voturi numarate: ${((t_v_v / (total_prezenta - total_n)) * 100).toFixed(4)}%`);
         }
       });
   });
